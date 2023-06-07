ccdd09b95af5b80dc00a729579724f10821e376e
rahibgasimli <rahib8425@gmail.com>
2023-06-07 18:02:55 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,68 @@
+
+const telebeler = [
+    {
+        ad:"Rahib",
+        soyad:"Qasimli",
+        age:19,
+        gpa:99.45,
+        isEducated:true
+    },
+    {
+        ad:"Selale",
+        soyad:"Verdiyeva",
+        age:17,
+        gpa:99.45,
+        isEducated:true
+    },
+    {
+        ad:"Aytac",
+        soyad:"Verdiyeva",
+        age:18,
+        gpa:99.45,
+        isEducated:true
+    },
+    {
+        ad:"Rashad",
+        soyad:"Aliyev",
+        age:22,
+        gpa:99.45,
+        isEducated:true
+    },
+    {
+        ad:"Beyali",
+        soyad:"Aliyev",
+        age:19,
+        gpa:99.45,
+        isEducated:true
+    },
+    {
+        ad:"Vaqif",
+        soyad:"Memmedov",
+        age:19,
+        gpa:99.45,
+        isEducated:true
+    }
+
+]
+
+
+
+const tbodyEl = document.getElementsByClassName("melumatlar")
+
+
+for(let i=0; i<telebeler.length; i++) {
+
+    tbodyEl.inn
+
+}
+    
+
+
+
+// <td>Qasimli</td>
+// <td>Rahib</td>
+// <td>19</td>
+// <td>99.45</td>
+// <td>Tehsillidir</td>
+// <td><i class="fa-solid fa-pen-to-square"></i></td>
+// <td><i class="fa-solid fa-trash"></i></td>
